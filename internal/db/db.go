b080541136f54ade3da4db458ef123d0a2cbd711
Firdaus Adi Putra <mail@firdaus.org>
2025-06-11 08:51:02 +0700

    Rename file

---
+++ internal/db/db.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func NewConnection() (*sql.DB, error) {
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+	sslmode := os.Getenv("POSTGRES_SSLMODE")
+
+	if host == "" || port == "" || user == "" || password == "" || dbname == "" || sslmode == "" {
+		return nil, fmt.Errorf("one or more required PostgreSQL environment variables are not set")
+	}
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Verify connection
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to PostgreSQL database:", dsn)
+	return db, nil
+}
